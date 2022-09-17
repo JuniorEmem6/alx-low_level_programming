8a2da47b91359947d69b6bb7718ddb2b27b34cd4
Emem Edem <simplyemem10@gmail.com>
2022-09-16 21:11:18 -0700

    added 1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite lopp
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop coming : (\n");
+
+	i = 0;
+       /**
+	* while (i < 10)
+	* {
+		putchar(i);
+	* }
+	*/
+	printf("Infinit loop avoided! \\0/\n");
+
+	return (0);
+}
