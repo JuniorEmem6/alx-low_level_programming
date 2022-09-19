900614ddef8820dff5ede5ae225a7913426fdfb0
Emem Edem <simplyemem10@gmail.com>
2022-09-19 16:30:43 -0700

    put 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be printed
+ *
+ */
+
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		printf("%c", str[index]);
+
+	printf("\n");
+
