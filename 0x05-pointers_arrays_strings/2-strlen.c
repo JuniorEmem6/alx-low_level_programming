0caf411257bc82a74bd91ca428797f53ca46b025
Emem Edem <simplyemem10@gmail.com>
2022-09-18 23:34:00 -0700

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * _strlen - returns the length of a string
  * @s: string to check for length
@@ -11,7 +11,7 @@ int _strlen(char *s)
 {
 	int n;
 
-	n = sizeof(s);
+	n = strlen(s);
 
 	return (n);
 }
